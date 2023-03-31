edd18d61ee237dac681504ae1266010195b12e9c
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-03-31 02:16:02 -0300

    Criação de componente.

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <div class="columns">
+            <div class="column is-7">
+                Descrição da tarefa
+            </div>
+            <div class="column">
+                <Cronometro :segundos="15" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
+
+export default defineComponent({
+    name: 'Tarefa',
+    emits: ['clique'],
+    components: { Cronometro },
+    methods: {}
+})
+</script>
+
+<style scoped>
+.box {
+    background: #f8fad6;
+}
+</style>
