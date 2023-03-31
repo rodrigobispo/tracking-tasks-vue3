20062ff523892be6df71fd4ddb26249bbdd3154a
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-03-31 08:22:50 -0300

    Mudança na forma de aplicação de estilo; a partir de data().

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,8 +1,10 @@
 <template>
-  <div :class="estilo">
+  <div v-if="msgAviso" class="box has-text-weight-bold" :style="estiloNormal">
     <slot></slot>
   </div>
-
+  <div v-else class="box has-text-weight-bold" :style="estiloSemTarefa">
+    <slot></slot>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,25 +12,21 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Box',
+  data: () => {
+    return {
+      estiloNormal: {
+        backgroundColor: '#ffd1d1' //padrão camel case
+      },
+      estiloSemTarefa: {
+        'background-color': '#f8fad6' //outra maneira
+      },
+    }
+  },
   props: {
     msgAviso: {
         type: Boolean,
         required: false
     }
   },
-  computed: {
-    estilo() {
-        return this.msgAviso ? 'box box-error has-text-weight-bold' : 'box has-text-weight-bold'
-    }
-  }
 })
 </script>
-
-<style scoped>
-.box {
-  background: #f8fad6;
-}
-.box.box-error {
-  background: #ffd1d1;
-}
-</style>
