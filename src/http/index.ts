11cec725b626ba2df6bfde556e3bc3aada26c133
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-04-14 12:00:48 -0300

    Atualização para endpoint vercel.

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios"
 
 const clienteHttp: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/"
+  baseURL: "https://json-server-vercel-tasktracker.vercel.app/"
 })
 
 export default clienteHttp
