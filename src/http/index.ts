febb9777e997993eed7f963851c601c87fdaecc6
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-04-21 04:28:26 -0300

    Url em variável de ambiente.

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,11 @@
 import axios, { AxiosInstance } from "axios"
 
 const clienteHttp: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/"
+  baseURL: process.env.VUE_APP_BASE_URL_AXIOS,
+  headers: {
+    "Content-type": "application/json",
+    // Authorization: `Bearer ${process.env.VUE_APP_TOKEN_DBJSON_TRACKER}`,
+  },
 })
 
 export default clienteHttp
